75c8ddcfe192c889fd141403be205888fcbedac2
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-08-01 10:32:54 +0530

    git

--- binary.py
+++ binary.py
@@ -1,7 +1,7 @@
 
 import cv2
 
-img = cv2.imread(r"hero.jpg", 0)  # Read image in grayscale
+img = cv2.imread("cat.jpg", 0)  # Read image in grayscale
 
 _, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
